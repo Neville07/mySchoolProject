bf449bd03dd7822196b06e0109ec096f79ce26fe
Neville07 <67411960+Neville07@users.noreply.github.com>
2021-09-22 13:50:15 +0700

    Create 1012.py

---
+++ uri/1012.py
@@ -0,0 +1,16 @@
+A,B,C = input().split()
+A = float(A)
+B = float(B)
+C = float(C)
+pi = 3.14159
+TRIANGULO = A*C/2
+CIRCULO = pi*C**2
+TRAPEZIO = (A+B)*C/2
+QUADRADO = B**2
+RETANGULO = A*B
+
+print(f"TRIANGULO: {TRIANGULO:.3f}")
+print(f"CIRCULO: {CIRCULO:.3f}")
+print(f"TRAPEZIO: {TRAPEZIO:.3f}")
+print(f"QUADRADO: {QUADRADO:.3f}")
+print(f"RETANGULO: {RETANGULO:.3f}")
